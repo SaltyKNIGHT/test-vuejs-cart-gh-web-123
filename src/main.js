7f2ab5e89698864de60dfce519c5979a3cb76c6a
zero_four <vincent.zack@hotmail.com>
2022-09-07 16:17:58 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// eslint-disable-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
+import './assets/styles/style.scss' // for side-effects only
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
